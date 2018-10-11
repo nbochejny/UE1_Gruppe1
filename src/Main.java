55bed0c81bea2b3b47f1e06e0b808d8f33537681
Bochejny Nicolas <1810653904@stud.fh-kufstein.ac.at>
2018-10-11 15:16:02 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        int alter= 20;
         String vorname= "Nicolas";
-        System.out.println("Hello " + vorname);
+        System.out.println("Hello " + vorname + " you are " + alter + " years old yo");
     }
 }
