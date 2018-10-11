85e8ae008f2274fcb0a651b89d6422a841d29a9c
Bochejny Nicolas <1810653904@stud.fh-kufstein.ac.at>
2018-10-11 15:39:20 +0200

    Nachname

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int alter= 20;
+        byte alter= 20;
         String vorname= "Nicolas";
-        System.out.println("Hello " + vorname + " you are " + alter + " years old yo");
+        String nachname= "Bochejny";
+        System.out.println("Hello " + vorname + " " + nachname + " you are " + alter + " years old yo");
     }
 }
